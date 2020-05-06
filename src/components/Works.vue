2e6221537fed46c00050269100c5a14d2bc7fe5f
HouraijiN <>
2020-05-06 23:08:39 +0900

    workのページのテンプレとスタイルを修正

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,64 +1,38 @@
 <template>
   <section class="bl_mainContents">
-    <ul class="bl_mediaList" id="js_mediaList">
-      <!-- ! To Do この部分をjson化したデータから読み込む -->
-      <!-- ! Memo プルリク欲しい -->
-      <li class="bl_mediaListItem">
-        <h1 class="bl_media_title">
-          異世界の秘密を曝くのはもう懲り懲りだよ〜!!!
-        </h1>
-        <div class="bl_media">
-          <div class="bl_media_imgWrapper">
-            <img src="./../assets/image/artwork/0014.jpg" alt="artwork" />
-          </div>
-          <div class="bl_media_body">
-            <ol class="bl_media_list">
-              <li class="bl_media_listItem">
-                <p>
-                  ふたりの
-                </p>
-                <p>
-                  (原曲 : 他愛も無い二人の博物誌)
-                </p>
-              </li>
-              <li class="bl_media_listItem">
-                <p>
-                  むかしむかし
-                </p>
-                <p>
-                  (原曲 : 童祭 ～ Innocent Treasures)
-                </p>
-              </li>
-              <li class="bl_media_listItem">
-                <p>
-                  おおぞら
-                </p>
-                <p>
-                  (原曲 : 天空のグリニッジ)
-                </p>
-              </li>
-              <li class="bl_media_listItem">
-                <p>
-                  ふたりで
-                </p>
-                <p>
-                  (原曲 : 少女秘封倶楽部)
-                </p>
-              </li>
-              <li class="bl_media_listItem">
-                <p>
-                  よる
-                </p>
-                <p>
-                  (原曲 : 夜のデンデラ野を逝く)
-                </p>
-              </li>
-            </ol>
-          </div>
+    <ul class="bl_mediaList">
+      <li
+        v-for="workListDetail in workList"
+        :key="workListDetail.id"
+        class="bl_mediaListItem"
+      >
+        <div class="bl_media_imgWrapper">
+          <img
+            v-bind:src="workListDetail.imageName"
+            v-bind:alt="workListDetail.id"
+          />
+        </div>
+
+        <!-- モーダルウィンドウで表示した方がいいか、別ページ遷移がいいか悩み中。 -->
+        <!-- 個人的にはモーダルにしたい。プルリク頼む〜〜〜 -->
+        <div class="bl_media_overlayText">
+          <p>
+            More...
+          </p>
         </div>
       </li>
     </ul>
   </section>
 </template>
 
-<script></script>
+<script>
+import workList from './../assets/workList.json'
+
+export default {
+  data () {
+    return {
+      workList: workList
+    }
+  }
+}
+</script>

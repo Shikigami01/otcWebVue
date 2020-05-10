61e2c5598a17868032119314296e2b96477e3dbe
HouraijiN <>
2020-05-10 23:42:53 +0900

    不要記述削除

--- src/main.js
+++ src/main.js
@@ -7,16 +7,6 @@ import './assets/style/main.scss'
 Vue.config.productionTip = false
 
 new Vue({
-  // ! To Do なぜか死んでる
-  // el: '#app',
-  // data () {
-  //   return {
-  //     info: null
-  //   }
-  // },
-  // mounted () {
-  //   axios.get('./assets/workList.json').then(response => (this.info = response))
-  // },
   router,
   render: h => h(App)
 }).$mount('#app')
